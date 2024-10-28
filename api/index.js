0b5032faeed8d7be68dce337c1283862f43a1950
0xzoowa <85619936+0xzoowa@users.noreply.github.com>
2024-10-28 09:44:52 +0100

    Update index.js II

--- api/index.js
+++ api/index.js
@@ -37,7 +37,9 @@ app.use("/api/profiles", profileRoutes);
 // Error handling middleware
 app.use(errorHandler);
 
-app.get("/", async (req, res) => {res.status(200).json(sucess: true, message: "This is blackbox api by 0xzoowa...")})
+app.get("/", async (req, res) => {
+  res.status(200).json({sucess: true, message: "This is blackbox api by 0xzoowa..."});
+})
 
 app.listen(PORT, () => {
   connectDB();
